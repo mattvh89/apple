67b18e80601f205f914082fa09f8ffe6b80663e1
mattvh89 <matt.van.helden@hotmail.com>
2025-03-05 18:32:50 -0500

    Update Debug.h

--- apple/Debug.h
+++ apple/Debug.h
@@ -4,7 +4,6 @@
 	//#define DEBUGGING_MODE 
 	#ifdef DEBUGGING_MODE
 		#include <iostream>
-		//std::ofstream DEBUG_FILE("logs/debug.dat", std::ios::out);
 		#define DEBUG_OUT(message) std::cout << message << std::endl
 	#else
 		#define DEBUG_OUT(message) if(false) false
